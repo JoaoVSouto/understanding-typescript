af6e3e0c2ac2a9996aaa68b3aca661c339358ef5
JoaoVSouto <joaovitorss99@hotmail.com>
2020-05-19 12:37:34 -0300

    :books: Adds studies of compilation arguments

---
+++ compiler/compiler.ts
@@ -0,0 +1,27 @@
+let channel: string = 'Gaveta';
+let subscribers: number = 610234;
+
+// channel = subscribers;
+console.log(channel);
+
+// let myName: string = 'Pedro';
+
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+let anything;
+anything = 12;
+anything = 'abc';
+
+function greet(isMorning: boolean): string {
+  let greeting: string;
+
+  if (isMorning) {
+    greeting = 'Good morning!';
+  } else {
+    greeting = 'Good evening!';
+  }
+
+  return greeting;
+}
