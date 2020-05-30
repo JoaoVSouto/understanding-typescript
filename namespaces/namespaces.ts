27db0bdab1bb1540f4f9eff5a6a29c90bd9cb3fc
JoaoVSouto <joaovitorss99@hotmail.com>
2020-05-30 11:54:34 -0300

    :books: Adds studies of namespaces

---
+++ namespaces/namespaces.ts
@@ -0,0 +1,9 @@
+/// <reference path="circGeometry.ts" />
+/// <reference path="rectGeometry.ts" />
+
+const PI = 2.99;
+
+console.log(Geometry.Area.circumference(10));
+console.log(Geometry.Area.rectangle(10, 20));
+
+console.log(PI);
